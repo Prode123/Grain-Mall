daf100b010ee0a5a495610fac49201d32d1655c0
Prode123 <1002411753@qq.com>
2023-12-12 20:50:13 +0800

    nacos注册中心和feign远程调用测试

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -2,7 +2,17 @@ package com.atguigu.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ *   远程调用使用
+ *   1)引入open-feign依赖
+ *   2)编写接口。告诉springcloud这个接口需要调用远程服务，声明接口的方法是要调用哪个远程服务的方法
+ *   3)开启远程调用注解@EnableFeignClients
+ **/
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
 
