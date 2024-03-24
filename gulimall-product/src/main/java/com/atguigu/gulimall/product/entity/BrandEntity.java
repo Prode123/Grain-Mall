67df3f65e52f437ce1e42c8562f406cf0046177f
Prode123 <1002411753@qq.com>
2024-03-24 22:08:06 +0800

    商品服务品牌管理JSR303自定义注解校验,新增按照状态修改品牌信息方法和校验注解分组

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
@@ -3,11 +3,13 @@ package com.atguigu.gulimall.product.entity;
 import com.atguigu.common.validator.ListValue;
 import com.atguigu.common.validator.group.AddGroup;
 import com.atguigu.common.validator.group.UpdateGroup;
+import com.atguigu.common.validator.group.UpdateStatusGroup;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
@@ -20,17 +22,13 @@ import javax.validation.constraints.*;
  * @author litong
  * @email 1002411753@qq.com
  * @date 2023-12-03 23:14:00
- *
  * @NotEmpty 、@NotBlank 和 @NotNull 这三个注解都是 Java Bean Validation API 中的一部分，主要用于对 Java 对象属性进行约束校验，以确保输入的有效性和完整性。以下是它们的具体区别：
- * @NotNull
- * 作用于任何非原始类型的字段，包括包装类型（如 Integer, String 等）以及引用类型。
+ * @NotNull 作用于任何非原始类型的字段，包括包装类型（如 Integer, String 等）以及引用类型。
  * 它仅检查字段值是否为 null，如果字段值为 null，则校验失败；但如果字段是一个字符串，即使这个字符串内容为空串（""），校验也会通过。
- * @NotEmpty
- * 主要应用于容器类型，例如 List, Set, Map 或数组，以及字符串 (String)。
+ * @NotEmpty 主要应用于容器类型，例如 List, Set, Map 或数组，以及字符串 (String)。
  * 对于集合类，它会检查集合本身是否为 null 以及其大小是否为零。
  * 对于字符串，它不仅检查字符串是否为 null，还会检查字符串的长度是否为零，也就是说，字符串不能为空串。
- * @NotBlank
- * 专门针对 String 类型设计，通常用于文本输入验证。
+ * @NotBlank 专门针对 String 类型设计，通常用于文本输入验证。
  * 不仅要求字符串不为 null，而且要求在去除两端空白字符（trim）之后，字符串的实际内容（非空白字符部分）长度必须大于零。
  * 所以，对于字符串而言，@NotBlank 比 @NotEmpty 更严格，因为它不允许有纯空白字符的字符串。
  * 总结：
@@ -69,8 +67,8 @@ public class BrandEntity  {
 	 * 显示状态[0-不显示；1-显示]
 	 */
 //	@ListValue(vals = {0,1},message = "显示状态[0-不显示；1-显示]必须是0或1",groups = AddGroup.class)
-	@ListValue(vals = {0,1},groups = AddGroup.class)
-	@NotNull(message = "显示状态[0-不显示；1-显示]不能为空")
+	@ListValue(vals = {0,1},groups = {AddGroup.class,UpdateStatusGroup.class})
+	@NotNull(message = "显示状态[0-不显示；1-显示]不能为空",groups = {AddGroup.class,UpdateStatusGroup.class})
 	private Integer showStatus;
 	/**
 	 * 检索首字母
