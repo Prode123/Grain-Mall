295f0b29e88e1899b6109d19eb1ee28eeb539f0b
Prode123 <1002411753@qq.com>
2024-03-27 22:07:41 +0800

    商品服务品牌管理新增brand_id精确查询和name模糊查询

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/BrandServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/BrandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.alibaba.cloud.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,6 +19,15 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<BrandEntity> queryWrapper = new QueryWrapper<>();
+        // 获取key值
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            // 模糊查询
+            // select * from pms_brand where brand_id = key or name like '%key%'
+            queryWrapper.eq("brand_id", key).or().like("name", key);
+        }
+
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
                 new QueryWrapper<BrandEntity>()
