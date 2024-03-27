da5e767ea4f3d10208976d3de9ebee020f41036a
Prode123 <1002411753@qq.com>
2024-03-27 22:28:04 +0800

    商品服务品牌管理新增关联关系名称保存

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryBrandRelationServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryBrandRelationServiceImpl.java
@@ -1,5 +1,8 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.dao.BrandDao;
+import com.atguigu.gulimall.product.dao.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -16,6 +19,11 @@ import com.atguigu.gulimall.product.service.CategoryBrandRelationService;
 @Service("categoryBrandRelationService")
 public class CategoryBrandRelationServiceImpl extends ServiceImpl<CategoryBrandRelationDao, CategoryBrandRelationEntity> implements CategoryBrandRelationService {
 
+    @Autowired
+    BrandDao brandDao;
+    @Autowired
+    CategoryDao categoryDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryBrandRelationEntity> page = this.page(
@@ -24,6 +32,20 @@ public class CategoryBrandRelationServiceImpl extends ServiceImpl<CategoryBrandR
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public void saveDetail(CategoryBrandRelationEntity categoryBrandRelation) {
+        Long brandId = categoryBrandRelation.getBrandId();
+        Long catelogId = categoryBrandRelation.getCatelogId();
+        //查询详细名字
+        String brandName = brandDao.selectById(brandId).getName();
+        String catelogName = categoryDao.selectById(catelogId).getName();
+
+        categoryBrandRelation.setBrandName(brandName);
+        categoryBrandRelation.setCatelogName(catelogName);
+
+        this.save(categoryBrandRelation);
     }
 
 }
