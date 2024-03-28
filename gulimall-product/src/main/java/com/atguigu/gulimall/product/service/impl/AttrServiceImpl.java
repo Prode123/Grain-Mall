71603729d46aac67e4116f2d51a874b1ddab80bc
Prode123 <1002411753@qq.com>
2024-03-28 21:54:55 +0800

    商品服务平台属性规格参数新增vo和级联新增方法

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.atguigu.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.atguigu.gulimall.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,11 +16,14 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.product.dao.AttrDao;
 import com.atguigu.gulimall.product.entity.AttrEntity;
 import com.atguigu.gulimall.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
 
+    @Autowired
+    AttrAttrgroupRelationDao relationDao;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<AttrEntity> page = this.page(
@@ -24,6 +32,21 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveAttr(AttrVo attrvo) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attrvo, attrEntity);
+        //保存基本数据
+        this.save(attrEntity);
+        //保存关联关系
+        AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+        attrAttrgroupRelationEntity.setAttrGroupId(attrvo.getAttrGroupId());
+        attrAttrgroupRelationEntity.setAttrId(attrEntity.getAttrId());
+        relationDao.insert(attrAttrgroupRelationEntity);
+
     }
 
 }
