65c50e9ce60ab18dd742a09b22bffcdfc6068a3f
Prode123 <1002411753@qq.com>
2024-04-01 23:21:05 +0800

    商品服务-新增商品-获取分类关联的品牌

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryBrandRelationController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryBrandRelationController.java
@@ -3,7 +3,10 @@ package com.atguigu.gulimall.product.controller;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.vo.BrandVo;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +31,24 @@ public class CategoryBrandRelationController {
     @Autowired
     private CategoryBrandRelationService categoryBrandRelationService;
 
+    /**
+     * @Description //product/categorybrandrelation/brans/list
+     * @Author LiTong(Prode)
+     * @Date 2024/04/01 23:09
+     **/
+    @GetMapping("/brans/list")
+    public R relationBrandsList(@RequestParam(value = "catId", required = true) Long catId) {
+        List<BrandEntity> entities = categoryBrandRelationService.getBrandsByCatId(catId);
+
+        List<BrandVo> collect = entities.stream().map(item -> {
+            BrandVo brandVo = new BrandVo();
+            brandVo.setBrandId(item.getBrandId());
+            brandVo.setBrandName(item.getName());
+            return brandVo;
+        }).collect(Collectors.toList());
+
+        return R.ok().put("data", collect);
+    }
     /**
      * 列表
      */
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/BrandVo.java
@@ -0,0 +1,19 @@
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author LiTong(Prode)
+ * @Data 2024/04/01 23:02
+ */
+@Data
+public class BrandVo {
+
+    private Long brandId;
+    private String brandName;
+//    private String brandImg;
+//    private String brandLetter;
+//    private String brandFirstLetter;
+//    private Integer showStatus;
+}
