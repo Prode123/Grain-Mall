047070ac89bb671ca374131e63a6ab7cc9db3ff7
Prode123 <1002411753@qq.com>
2024-04-07 21:46:10 +0800

    仓储服务-仓库管理-合并采购需求

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/PurchaseController.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/PurchaseController.java
@@ -3,12 +3,9 @@ package com.atguigu.gulimall.ware.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.atguigu.gulimall.ware.vo.MergeVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gulimall.ware.entity.PurchaseEntity;
 import com.atguigu.gulimall.ware.service.PurchaseService;
@@ -29,6 +26,22 @@ import com.atguigu.common.utils.R;
 public class PurchaseController {
     @Autowired
     private PurchaseService purchaseService;
+
+
+    ///ware/purchase/unreceive/list
+    @GetMapping("/unreceive/list")
+    public R unreceiveList(@RequestParam Map<String, Object> params){
+        PageUtils page = purchaseService.queryPageUnreceivePurchase(params);
+
+        return R.ok().put("page", page);
+    }
+
+    //ware/purchase/merge
+    @PostMapping("/merge")
+    public R merge(@RequestBody MergeVo mergeVo){
+        purchaseService.mergePurchase(mergeVo);
+        return R.ok();
+    }
 
     /**
      * 列表
--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseServiceImpl.java
@@ -1,7 +1,17 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import com.atguigu.common.constant.WareConstant;
+import com.atguigu.gulimall.ware.entity.PurchaseDetailEntity;
+import com.atguigu.gulimall.ware.service.PurchaseDetailService;
+import com.atguigu.gulimall.ware.vo.MergeVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,11 +21,14 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.ware.dao.PurchaseDao;
 import com.atguigu.gulimall.ware.entity.PurchaseEntity;
 import com.atguigu.gulimall.ware.service.PurchaseService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("purchaseService")
 public class PurchaseServiceImpl extends ServiceImpl<PurchaseDao, PurchaseEntity> implements PurchaseService {
 
+    @Autowired
+    PurchaseDetailService purchaseDetailService;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<PurchaseEntity> page = this.page(
@@ -24,6 +37,48 @@ public class PurchaseServiceImpl extends ServiceImpl<PurchaseDao, PurchaseEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPageUnreceivePurchase(Map<String, Object> params) {
+
+        IPage<PurchaseEntity> page = this.page(
+                new Query<PurchaseEntity>().getPage(params),
+                new QueryWrapper<PurchaseEntity>().
+                        eq("status",0).or().
+                        eq("status",1)
+        );
+
+        return new PageUtils(page);
+    }
+
+    @Transactional
+    @Override
+    public void mergePurchase(MergeVo mergeVo) {
+        Long purchaseId = mergeVo.getPurchaseId();
+        if(purchaseId == null){
+            PurchaseEntity purchaseEntity = new PurchaseEntity();
+            purchaseEntity.setStatus(WareConstant.PurchaseStatusEnum.CREATED.getCode());
+            purchaseEntity.setCreateTime(new Date());
+            purchaseEntity.setUpdateTime(new Date());
+            this.save(purchaseEntity);
+            purchaseId = purchaseEntity.getId();
+        }
+        List<Long> items = mergeVo.getItems();
+        Long finalPurchaseId = purchaseId;
+        List<PurchaseDetailEntity> collect = items.stream().map(i -> {
+            PurchaseDetailEntity detailEntity = new PurchaseDetailEntity();
+            detailEntity.setId(i);
+            detailEntity.setPurchaseId(finalPurchaseId);
+            detailEntity.setStatus(WareConstant.PurchaseDetailStatusEnum.ASSIGNED.getCode());
+            return detailEntity;
+        }).collect(Collectors.toList());
+        purchaseDetailService.updateBatchById(collect);
+
+        PurchaseEntity purchaseEntity = new PurchaseEntity();
+        purchaseEntity.setId(purchaseId);
+        purchaseEntity.setUpdateTime(new Date());
+        this.updateById(purchaseEntity);
     }
 
 }
