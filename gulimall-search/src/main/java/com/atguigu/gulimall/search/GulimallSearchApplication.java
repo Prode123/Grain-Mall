27f2e397e6de26772a6c115b50e6be20bb6687b3
Prode123 <1002411753@qq.com>
2024-04-11 23:09:35 +0800

    ElasticSearch测试方法

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/GulimallSearchApplication.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/GulimallSearchApplication.java
@@ -1,12 +1,17 @@
 package com.atguigu.gulimall.search;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
+@EnableConfigurationProperties
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan("com.atguigu.gulimall.search.config")
 public class GulimallSearchApplication {
 
     public static void main(String[] args) {
--- gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -1,12 +1,12 @@
 package com.atguigu.gulimall.search.config;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.*;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import static com.alibaba.nacos.api.common.Constants.TOKEN;
 
 /**
  * @Description
@@ -18,17 +18,26 @@ public class GulimallElasticSearchConfig {
 
 
     @Value("${es.hostname}")
-    private String hostname;
+    private static String hostname;
     @Value("${es.port}")
-    private int port;
+    private static int port;
     @Value("${es.scheme}")
-    private String scheme;
+    private static String scheme;
+    public static RequestOptions COMMON_OPTIONS;
 
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization","Bearer "+ TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
 
-    public RestHighLevelClient esRestClient() {
+    public static RestHighLevelClient esRestClient() {
        RestHighLevelClient client = new RestHighLevelClient(
                RestClient.builder(
-                       new HttpHost(hostname, port, scheme)
+                       new HttpHost("123.56.181.42", Integer.parseInt("9200"), "http")
                )
        );
                return client;
