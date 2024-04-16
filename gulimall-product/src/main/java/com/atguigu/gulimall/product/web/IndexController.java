424a3bbb00d64e08b1b7506c484e67f5224e836e
Prode123 <1002411753@qq.com>
2024-04-16 20:04:05 +0800

    商城业务-首页-整合dev-tools渲染数据

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -0,0 +1,34 @@
+package com.atguigu.gulimall.product.web;
+
+import com.atguigu.gulimall.product.entity.CategoryEntity;
+import com.atguigu.gulimall.product.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author LiTong(Prode)
+ * @Data 2024/04/16 19:18
+ */
+@RestController
+public class IndexController {
+
+    @Autowired
+    CategoryService categoryService;
+
+//    @GetMapping({"/","index.html"})
+//    public String indexPage(Model model){
+//        //查出所有的一级分类
+//        List<CategoryEntity> categoryEntities =  categoryService.getLevel1Categorys();
+//        model.addAttribute("categorys",categoryEntities);
+//        //视图解析器：classpath:/templates/ + 返回值 + .html
+////        return "classpath:/templates/index.html";
+//        return "index";
+//    }
+
+
+}
