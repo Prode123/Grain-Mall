e795d93dfe28b2200c4752519780db83c6cc8e6f
Prode123 <1002411753@qq.com>
2024-12-26 11:22:13 +0800

    Redisson-看门狗机制以及使用Redisson时最佳实践

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description
@@ -51,12 +52,21 @@ public class IndexController {
 //        获取一把锁，只要锁的名字一样，就是同一把锁
         RLock myLock = redisson.getLock("myLock");
 //        加锁
-        myLock.lock();// 阻塞式等待 默认加的锁都是30s时间
+//        myLock.lock();// 阻塞式等待 默认加的锁都是30s时间
 //        1、锁的自动续期，如果业务超长，运行期间自动给锁续上新的30s，不用担心业务时间长，锁自动过期被删掉
 //        2、加锁的业务只要运行完成，就不会给当前锁续期，即使不手动解锁，锁默认在30s以后自动删除
+        myLock.lock(10, TimeUnit.SECONDS);// 10秒自动解锁，自动解锁时间一定要大于业务执行时间
+//        问题：lock.lock(10, TimeUnit.SECONDS);在锁时间到了以后，不会自动续期
+//        1、如果传递了锁的超时时间，就发送给redis执行脚本，进行占锁，默认超时就是我们指定的时间
+//        2如果未指定锁的超时时间，就使用30 * 1000【LockWatchdogTimeout看门狗的默认时间】
+//          只要占锁成功，就会启动一个定时任务【重新给锁设置过期时间，新的过期时间就是看门狗的默认时间】,
+//          LockWatchdogTimeout看门狗的默认时间/3 = 10s
+
+//        最佳实战
+//        myLock.lock(10, TimeUnit.SECONDS);可以省掉续期操作 手动解锁
         try {
             System.out.println("加锁成功，执行业务。。。。。"+Thread.currentThread().getId());
-            Thread.sleep(10000);
+            Thread.sleep(30000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
